1fa25d01aa2f76862992d97c1af9c8af56824366
Vdujour <valentin.dujour@etu.univ-poitiers.fr>
2025-12-09 16:08:08 +0100

    fix: black start date at 01-01-2023

--- public/clinic/main.js
+++ public/clinic/main.js
@@ -26,21 +26,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const { startDate, endDate } = getLastMonthDateRange();
 
-    // Initialiser les champs de date avec les valeurs du mois précédent
-    document.getElementById('start-date').value = startDate;
+    // Initialiser les champs de date avec les valeurs personnalisées
+    document.getElementById('start-date').value = '2023-01-01';
     document.getElementById('end-date').value = endDate;
     
-    // Charger les données pour le mois précédent
-    loadStats(startDate, endDate);
-    loadMedecinsData(startDate, endDate);
-    loadVisitsRevenue(startDate, endDate);
-    loadChartData(startDate, endDate);
-    loadHourlyRevenueHeatmap(startDate, endDate);
-    loadPatientsHeatmap(startDate, endDate);
-    loadActesData(startDate, endDate);
-    loadWaitingTimes(startDate, endDate);
-    loadWaitingTimeHeatmap(startDate, endDate);
-    loadStackedBarData(startDate, endDate);
+    // Charger les données à partir du 01/01/2023
+    loadStats('2023-01-01', endDate);
+    loadMedecinsData('2023-01-01', endDate);
+    loadVisitsRevenue('2023-01-01', endDate);
+    loadChartData('2023-01-01', endDate);
+    loadHourlyRevenueHeatmap('2023-01-01', endDate);
+    loadPatientsHeatmap('2023-01-01', endDate);
+    loadActesData('2023-01-01', endDate);
+    loadWaitingTimes('2023-01-01', endDate);
+    loadWaitingTimeHeatmap('2023-01-01', endDate);
+    loadStackedBarData('2023-01-01', endDate);
 
     // Ajouter un écouteur pour le bouton "Appliquer"
     document.getElementById('apply-period').addEventListener('click', function () {
